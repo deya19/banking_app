a908c789825b988fb05522fa3a118470afeb5520
deya19 <deyasaleh2@gmail.com>
2024-05-26 19:08:32 +0300

    new add

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -1,43 +1,33 @@
 "use server";
 
+
 import { Client, Account, Databases, Users } from "node-appwrite";
 import { cookies } from "next/headers";
 
 export async function createSessionClient() {
-  const client = new Client();
+  const client = new Client()
+    .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!)
+    .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT!);
 
-  client
-    .setEndpoint(`${process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!}`)
-    .setProject(`${process.env.NEXT_PUBLIC_APPWRITE_PROJECT!}`);
-
-  const sessionCookie = cookies().get("appwrite-session");
-
-  if (!sessionCookie || !sessionCookie.value) {
-    console.error("No session cookie found");
-    throw new Error("No session cookie found. Please log in.");
+  const session = cookies().get("appwrite-session");
+  if (!session || !session.value) {
+    throw new Error("No session");
   }
 
-  try {
-    client.setSession(sessionCookie.value);
+  client.setSession(session.value);
 
-    return {
-      get account() {
-        return new Account(client);
-      },
-    };
-  } catch (error) {
-    console.error("Error setting session:", error);
-    throw new Error("Failed to set session. Please try again.");
-  }
+  return {
+    get account() {
+      return new Account(client);
+    },
+  };
 }
 
 export async function createAdminClient() {
-  const client = new Client();
-
-  client
-    .setEndpoint(`${process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!}`)
-    .setProject(`${process.env.NEXT_PUBLIC_APPWRITE_PROJECT!}`)
-    .setKey(`${process.env.NEXT_APPWRITE_KEY!}`);
+  const client = new Client()
+    .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!)
+    .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT!)
+    .setKey(process.env.NEXT_APPWRITE_KEY!);
 
   return {
     get account() {
